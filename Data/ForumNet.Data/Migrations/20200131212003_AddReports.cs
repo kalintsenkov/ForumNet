9a1c697876a4b9fae7341d5051aa7e5c54a4a121
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-01-31 23:24:27 +0200

    Update Migrations

---
+++ Data/ForumNet.Data/Migrations/20200131212003_AddReports.cs
@@ -0,0 +1,82 @@
+﻿namespace ForumNet.Data.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddReports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Replies",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(maxLength: 1000, nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    PostId = table.Column<int>(nullable: false),
+                    AuthorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reports_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reports_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_AuthorId",
+                table: "Reports",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_PostId",
+                table: "Reports",
+                column: "PostId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Replies");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Posts");
+        }
+    }
+}
