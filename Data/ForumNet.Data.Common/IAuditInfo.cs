5ca473589a945839565c5e98b5e47d66825d0850
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-02-04 13:34:02 +0200

    Add IAuditInfo.cs and IDeletableEntity.cs

---
+++ Data/ForumNet.Data.Common/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ForumNet.Data.Common
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ Data/ForumNet.Data.Common/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ForumNet.Data.Common
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
