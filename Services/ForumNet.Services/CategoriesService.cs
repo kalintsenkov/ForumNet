80ce9d4e2292eed3dba2caeac5d986966929b196
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-02-20 21:14:36 +0200

    Add services folder

---
+++ Services/ForumNet.Services/CategoriesService.cs
@@ -0,0 +1,31 @@
+﻿namespace ForumNet.Services
+{
+    using System;
+    using System.Threading.Tasks;
+    using Contracts;
+    using Data;
+    using Data.Models;
+
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly ForumDbContext db;
+
+        public CategoriesService(ForumDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task Add(string name)
+        {
+            var category = new Category
+            {
+                Name = name,
+                CreatedOn = DateTime.UtcNow,
+                ModifiedOn = DateTime.UtcNow
+            };
+
+            await this.db.Categories.AddAsync(category);
+            await this.db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Services/ForumNet.Services/Contracts/ICategoriesService.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumNet.Services.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface ICategoriesService
+    {
+        Task Add(string name);
+    }
+}
