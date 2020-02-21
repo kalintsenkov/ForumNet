7cbd620b654efd731558ab1bdf119eedc110cc9c
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-02-21 19:20:01 +0200

    Add Forum.Web folders

---
+++ Web/ForumNet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ForumNet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
