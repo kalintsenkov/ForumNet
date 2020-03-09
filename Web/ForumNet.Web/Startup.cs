106adbbcc6dc4d7cba993b0b34cb6ab198556350
kalintsenkov <kalintsenkov3@gmail.com>
2020-03-09 20:22:43 +0200

    Add radio buttons in posts create view

--- Web/ForumNet.Web/Startup.cs
+++ Web/ForumNet.Web/Startup.cs
@@ -89,12 +89,12 @@ namespace ForumNet.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "areaRoute",
-                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "areaRoute",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapRazorPages();
             });
