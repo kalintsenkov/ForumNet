d0aa1f9b1c8137d4f02d28af699cf01afa350d22
kalintsenkov <kalintsenkov3@gmail.com>
2020-03-21 23:24:20 +0200

    Add ForumNet.Web.Tests

---
+++ ForumNet.Web.Tests/TestStartup.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumNet.Web.Tests
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MyTested.AspNetCore.Mvc;
+
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) 
+            : base(configuration)
+        {
+        }
+
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+            //// Replace only your own custom services. The ASP.NET Core ones 
+            //// are already replaced by MyTested.AspNetCore.Mvc. 
+            // services.Replace<IService, MockedService>()
+        }
+    }
+}
