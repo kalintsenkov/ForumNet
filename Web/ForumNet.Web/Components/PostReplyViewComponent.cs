383428cc23c14d36bc7de6275d2db2c059ffbc9e
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-03-24 17:25:52 +0200

    Add more partial views

---
+++ Web/ForumNet.Web/Components/PostReplyViewComponent.cs
@@ -0,0 +1,27 @@
+﻿namespace ForumNet.Web.Components
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Infrastructure.Extensions;
+    using Services.Contracts;
+
+    [ViewComponent(Name = "PostReply")]
+    public class PostReplyViewComponent : ViewComponent
+    {
+        private readonly IRepliesService repliesService;
+
+        public PostReplyViewComponent(IRepliesService repliesService)
+        {
+            this.repliesService = repliesService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string description, int? parentId, int postId)
+        {
+            await this.repliesService.CreateAsync(description, parentId, postId, this.UserClaimsPrincipal.GetId());
+
+            return this.View();
+        }
+    }
+}
