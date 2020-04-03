7060772150ac0c3f37d6091d1190404e74f8161d
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-04-03 12:31:46 +0300

    Fix dropdown bug

--- Web/ForumNet.Web.ViewModels/Users/UserLoginStatusViewModel.cs
+++ Web/ForumNet.Web.ViewModels/Users/UserLoginStatusViewModel.cs
@@ -2,6 +2,8 @@
 {
     public class UserLoginStatusViewModel
     {
+        public string Id { get; set; }
+
         public string UserName { get; set; }
 
         public string ProfilePicture { get; set; }
